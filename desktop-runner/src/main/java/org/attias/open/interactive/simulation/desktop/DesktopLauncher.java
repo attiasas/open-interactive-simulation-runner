094c5c00b44f741f60250ceca5b02a2513582929
Assaf Attias <assafattias93@gmail.com>
2023-08-31 20:43:01 +0300

    add android platform support

--- desktop-runner/src/main/java/org/attias/open/interactive/simulation/desktop/DesktopLauncher.java
+++ desktop-runner/src/main/java/org/attias/open/interactive/simulation/desktop/DesktopLauncher.java
@@ -8,6 +8,7 @@ import org.attias.open.interactive.simulation.core.backend.engine.InteractiveSim
 import org.attias.open.interactive.simulation.core.backend.utils.ProjectUtils;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class DesktopLauncher {
     public static void main (String[] arg) throws IOException {
@@ -16,14 +17,16 @@ public class DesktopLauncher {
     }
 
     public static AppConfiguration getConfigurations(LwjglApplicationConfiguration config) throws IOException {
-        AppConfiguration appConfiguration = AppConfiguration.getRunnerConfigurations();
+        try (InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(ProjectUtils.getProjectConfigurationsAssetsPath())) {
+            AppConfiguration appConfiguration = AppConfiguration.getRunnerConfigurations(in);
 
-        appConfiguration.setType(AppConfiguration.AppType.Desktop);
-        config.title = appConfiguration.getProjectConfiguration().title;
+            appConfiguration.setType(AppConfiguration.AppType.Desktop);
+            config.title = appConfiguration.getProjectConfiguration().title;
 
-        addIcons(config);
+            addIcons(config);
 
-        return appConfiguration;
+            return appConfiguration;
+        }
     }
 
     public static void addIcons(LwjglApplicationConfiguration config) {
@@ -41,7 +44,7 @@ public class DesktopLauncher {
         for (int size : sizes) {
             try {
                 System.out.println("loading icon" + extension + ": " + size);
-                config.addIcon(ProjectUtils.OIS_DIRECTORY_NAME + "/icons/icon" + size + extension, Files.FileType.Internal);
+                config.addIcon(ProjectUtils.OIS + "/icons/icon" + size + extension, Files.FileType.Internal);
             } catch (Exception e) {
                 System.out.println("found exception");
             }
